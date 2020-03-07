240157044120ccc36e7a4bd8accf34e7700fe32f
maxiwebs <maxiwebs@gmail.com>
2020-03-06 21:55:43 -0300

    Version jugable sin movimientos.

--- Codigo_TaTeTi.ino
+++ Codigo_TaTeTi.ino
@@ -74,6 +74,11 @@ byte pines_filas[FILASTECLADO] = {F1, F2, F3, F4};
 byte pines_columnas[COLUMNASTECLADO] = {C1, C2, C3}; 
 Keypad keypad = Keypad( makeKeymap(teclado_matricial), pines_filas, pines_columnas, FILASTECLADO, COLUMNASTECLADO);
 
+int jugadorActual = 0;
+char teclaPresionada;
+bool alguienGano = false;
+char ganador = 'X';
+int cantidadMovimientos = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -85,8 +90,299 @@ void loop() {
 
   //testLedsRGB();
   //testTeclado();
- 
+
+  while (!alguienGano){
+    mostrarTurnoJugador(jugadorActual);
+    imprimirTablero();
+    teclaPresionada = escucharTeclado();
+    cantidadMovimientos++;
+    Serial.print("Volvi con tecla: "); Serial.println(teclaPresionada);
+    prenderLedJugado(jugadorActual,teclaPresionada);
+    alguienGano = chequearVictoria(cantidadMovimientos);
+    siguienteTurno(jugadorActual);  
+  }
+
+  mostrarGanador();
+  Serial.println("FIN de JUEGO");
 }
+
+//FUNCIONES DE JUEGO
+
+//Prendo todos los leds con el color del ganador
+void mostrarGanador(){
+
+  if (ganador == 'R'){
+    digitalWrite(R11, HIGH); digitalWrite(G11, LOW); digitalWrite(B11, LOW);
+    digitalWrite(R12, HIGH); digitalWrite(G12, LOW); digitalWrite(B12, LOW);
+    digitalWrite(R13, HIGH); digitalWrite(G13, LOW); digitalWrite(B13, LOW);
+    digitalWrite(R21, HIGH); digitalWrite(G21, LOW); digitalWrite(B21, LOW);
+    digitalWrite(R22, HIGH); digitalWrite(G22, LOW); digitalWrite(B22, LOW);
+    digitalWrite(R23, HIGH); digitalWrite(G23, LOW); digitalWrite(B23, LOW);
+    digitalWrite(R31, HIGH); digitalWrite(G31, LOW); digitalWrite(B31, LOW);
+    digitalWrite(R32, HIGH); digitalWrite(G32, LOW); digitalWrite(B32, LOW);
+    digitalWrite(R33, HIGH); digitalWrite(G33, LOW); digitalWrite(B33, LOW);  
+  }else if (ganador == 'V'){
+    digitalWrite(R11, LOW); digitalWrite(G11, HIGH); digitalWrite(B11, LOW);
+    digitalWrite(R12, LOW); digitalWrite(G12, HIGH); digitalWrite(B12, LOW);
+    digitalWrite(R13, LOW); digitalWrite(G13, HIGH); digitalWrite(B13, LOW);
+    digitalWrite(R21, LOW); digitalWrite(G21, HIGH); digitalWrite(B21, LOW);
+    digitalWrite(R22, LOW); digitalWrite(G22, HIGH); digitalWrite(B22, LOW);
+    digitalWrite(R23, LOW); digitalWrite(G23, HIGH); digitalWrite(B23, LOW);
+    digitalWrite(R31, LOW); digitalWrite(G31, HIGH); digitalWrite(B31, LOW);
+    digitalWrite(R32, LOW); digitalWrite(G32, HIGH); digitalWrite(B32, LOW);
+    digitalWrite(R33, LOW); digitalWrite(G33, HIGH); digitalWrite(B33, LOW);
+  }
+  delay(300);
+  //Apago todos
+  digitalWrite(R11, LOW); digitalWrite(G11, LOW); digitalWrite(B11, LOW);
+  digitalWrite(R12, LOW); digitalWrite(G12, LOW); digitalWrite(B12, LOW);
+  digitalWrite(R13, LOW); digitalWrite(G13, LOW); digitalWrite(B13, LOW);
+  digitalWrite(R21, LOW); digitalWrite(G21, LOW); digitalWrite(B21, LOW);
+  digitalWrite(R22, LOW); digitalWrite(G22, LOW); digitalWrite(B22, LOW);
+  digitalWrite(R23, LOW); digitalWrite(G23, LOW); digitalWrite(B23, LOW);
+  digitalWrite(R31, LOW); digitalWrite(G31, LOW); digitalWrite(B31, LOW);
+  digitalWrite(R32, LOW); digitalWrite(G32, LOW); digitalWrite(B32, LOW);
+  digitalWrite(R33, LOW); digitalWrite(G33, LOW); digitalWrite(B33, LOW);
+  delay(300);
+
+}
+
+void imprimirTablero(){
+  
+  for (int i = 0; i<3 ; i++){
+    for (int j = 0; j<3; j++){
+      Serial.print(tateti[i][j]); Serial.print(",");
+    }
+    Serial.println("");
+  }
+
+}
+
+bool chequearVictoria(int cantMovs){
+
+  //Si no hay al menos 5 fichas jugadas, nadie gano
+  if (cantMovs <= 4) return false;
+
+  //Las posibilidades de ganar con led 11
+  if (tateti[0][0] == tateti[0][1] && tateti[0][1] == tateti[0][2]){ //F1
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[0][0]);
+    ganador = tateti[0][0];
+    return true;
+  }else if (tateti[0][0] == tateti[1][0] && tateti[1][0] == tateti[2][0]){ //C1
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[0][0]);
+    ganador = tateti[0][0];
+    return true; 
+  }else if (tateti[0][0] == tateti[1][1] && tateti[1][1] == tateti[2][2]){ //D1
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[0][0]);
+    ganador = tateti[0][0];
+    return true; 
+  }else if (tateti[1][0] == tateti[1][1] && tateti[1][1] == tateti[1][2]){ //F2
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[1][0]);
+    ganador = tateti[1][0];
+    return true; 
+  }else if (tateti[2][0] == tateti[2][1] && tateti[2][1] == tateti[2][2]){ //F3
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[2][0]);
+    ganador = tateti[2][0];
+    return true;
+  }else if (tateti[2][0] == tateti[1][1] && tateti[1][1] == tateti[0][2]){ //D2
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[2][0]);
+    ganador = tateti[2][0];
+    return true; 
+  }else if (tateti[0][1] == tateti[1][1] && tateti[1][1] == tateti[2][1]){ //C2
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[0][1]);
+    ganador = tateti[0][1];
+    return true; 
+  }else if (tateti[0][2] == tateti[1][2] && tateti[1][2] == tateti[2][2]){ //C3
+    Serial.print("Hay ganador!!!"); Serial.println(tateti[0][2]);
+    ganador = tateti[0][2];
+    return true; 
+  }else {
+    return false;
+  }
+      
+  
+
+}
+
+void siguienteTurno(int jugActual){
+  jugadorActual = (jugActual+1)%2;
+  Serial.print("Turno de: "); Serial.println(jugadorActual);
+}
+
+void prenderLedJugado(int jugActual, char teclaPres){
+
+  //O: rojo, 1: verde
+
+  switch (teclaPres){
+    case '1':
+      if (jugActual == 0){
+        //Prendo 1 rojo
+        digitalWrite(R11, HIGH); digitalWrite(G11, LOW); digitalWrite(B11, LOW);
+        tateti[0][0] = 'R';
+      }else {
+        //Prendo 1 verde
+        digitalWrite(R11, LOW); digitalWrite(G11, HIGH); digitalWrite(B11, LOW);    
+        tateti[0][0] = 'V';
+      }
+      break;
+
+    case '2':
+      if (jugActual == 0){
+        //Prendo 2 rojo
+        digitalWrite(R12, HIGH); digitalWrite(G12, LOW); digitalWrite(B12, LOW);
+        tateti[0][1] = 'R';
+      }else {
+        //Prendo 2 verde
+        digitalWrite(R12, LOW); digitalWrite(G12, HIGH); digitalWrite(B12, LOW);    
+        tateti[0][1] = 'V';
+      }
+      break;  
+    case '3':
+      if (jugActual == 0){
+        //Prendo 3 rojo
+        digitalWrite(R13, HIGH); digitalWrite(G13, LOW); digitalWrite(B13, LOW);
+        tateti[0][2] = 'R';
+      }else {
+        //Prendo 3 verde
+        digitalWrite(R13, LOW); digitalWrite(G13, HIGH); digitalWrite(B13, LOW);    
+        tateti[0][2] = 'V';
+      }
+      break;
+      
+    case '4':
+      if (jugActual == 0){
+        //Prendo 4 rojo
+        digitalWrite(R21, HIGH); digitalWrite(G21, LOW); digitalWrite(B21, LOW);
+        tateti[1][0] = 'R';
+      }else {
+        //Prendo 4 verde
+        digitalWrite(R21, LOW); digitalWrite(G21, HIGH); digitalWrite(B21, LOW);    
+        tateti[1][0] = 'V';
+      }
+      break;
+      
+    case '5':
+      if (jugActual == 0){
+        //Prendo 5 rojo
+        digitalWrite(R22, HIGH); digitalWrite(G22, LOW); digitalWrite(B22, LOW);
+        tateti[1][1] = 'R';
+      }else {
+        //Prendo 5 verde
+        digitalWrite(R22, LOW); digitalWrite(G22, HIGH); digitalWrite(B22, LOW);    
+        tateti[1][1] = 'V';
+      }
+      break;
+      
+    case '6':
+      if (jugActual == 0){
+        //Prendo 6 rojo
+        digitalWrite(R23, HIGH); digitalWrite(G23, LOW); digitalWrite(B23, LOW);
+        tateti[1][2] = 'R';
+      }else {
+        //Prendo 6 verde
+        digitalWrite(R23, LOW); digitalWrite(G23, HIGH); digitalWrite(B23, LOW);    
+        tateti[1][2] = 'V';
+      }
+      break;
+      
+    case '7':
+      if (jugActual == 0){
+        //Prendo 7 rojo
+        digitalWrite(R31, HIGH); digitalWrite(G31, LOW); digitalWrite(B31, LOW);
+        tateti[2][0] = 'R';
+      }else {
+        //Prendo 7 verde
+        digitalWrite(R31, LOW); digitalWrite(G31, HIGH); digitalWrite(B31, LOW);    
+        tateti[2][0] = 'V';
+      }
+      break;
+        
+    case '8':
+      if (jugActual == 0){
+        //Prendo 8 rojo
+        digitalWrite(R32, HIGH); digitalWrite(G32, LOW); digitalWrite(B32, LOW);
+        tateti[2][1] = 'R';
+      }else {
+        //Prendo 8 verde
+        digitalWrite(R32, LOW); digitalWrite(G32, HIGH); digitalWrite(B32, LOW);    
+        tateti[2][1] = 'V';
+      }
+      break;
+
+    case '9':
+      if (jugActual == 0){
+        //Prendo 9 rojo
+        digitalWrite(R33, HIGH); digitalWrite(G33, LOW); digitalWrite(B33, LOW);
+        tateti[2][2] = 'R';
+      }else {
+        //Prendo 9 verde
+        digitalWrite(R33, LOW); digitalWrite(G33, HIGH); digitalWrite(B33, LOW);    
+        tateti[2][2] = 'V';
+      }  
+      break;
+      
+  }//FIN switch(teclaPres)
+}
+
+char escucharTeclado(){
+
+  char presionada = NO_KEY;
+  char key  = NO_KEY;
+
+  //Mientras no se apriete una tecla
+  while (key == NO_KEY){
+    //Me quedo esperando
+    Serial.println("Presione una tecla y luego confirme con #");
+    presionada = keypad.getKey();
+
+    //Si presionaron una tecla y no es #
+    if (presionada != NO_KEY && presionada != '#'){
+      delay(100);
+      //Muestro la tecla presionada
+      Serial.println(presionada);
+      //Espero confirmacion (#)
+      key = keypad.getKey();
+
+      //Mientras no presionen confirmacion o cambien de opinion
+      while (key == NO_KEY){
+        Serial.println("Presione # para confirmar.");
+        key = keypad.getKey();
+        //Si apretaron una tecla y es la confirmacion
+        if (key != NO_KEY && key == '#'){
+          //Devuelvo la tecla presionada
+          return presionada;
+
+         //Si cambiaron de opinion y no confirmaron (pero apretaron otra tecla)
+        }else if (key != NO_KEY){
+          //Actualizo la tecla presionada
+          presionada = key;
+          //Muestro nueva tecla presionada
+          Serial.println(presionada);
+          //Seteo vacio para esperar confirmacion (#)
+          key = NO_KEY;
+        }
+        delay(100);
+      }
+      
+    }
+    delay(100);
+  }
+}
+
+void mostrarTurnoJugador(int actual){
+  
+  if (actual == 0){
+    //muestro color Rojo en el led del turno
+    digitalWrite(RLT, HIGH); digitalWrite(BLT, LOW); digitalWrite(GLT, LOW);
+  }else{
+    //muestro color Verde en el led del turno
+    digitalWrite(GLT, HIGH); digitalWrite(BLT, LOW); digitalWrite(RLT, LOW);
+  }
+
+}
+
+
+//FIN FUNCIONES DE JUEGO
 
 
 //Test del Teclado
